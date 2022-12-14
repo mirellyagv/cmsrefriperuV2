06eaa6e2302c49bfe70efb0820ad633180b4dc8a
mirellyagv <mirellyagv@gmail.com>
2022-12-14 02:49:13 -0500

    envio de correo

--- resources/views/pages/incidencia/index.blade.php
+++ resources/views/pages/incidencia/index.blade.php
@@ -27,11 +27,11 @@
                     <table border="0" cellspacing="5" cellpadding="5">
                         <tbody>
                             <tr>
-                                <td>Minimum date:</td>
+                                <td>Fecha inicio:</td>
                                 <td><input type="text" id="min" name="min"></td>
                             </tr>
                             <tr>
-                                <td>Maximum date:</td>
+                                <td>Fecha fin:</td>
                                 <td><input type="text" id="max" name="max"></td>
                             </tr>
                         </tbody>
