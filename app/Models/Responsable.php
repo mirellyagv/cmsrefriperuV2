cc99e3b6abfbda2418f2bf5eef86cc074e2f0d7a
bgalvanguerrero <bgalvanguerrero@gmail.com>
2023-02-27 11:48:17 -0500

    Update Responsable.php

--- app/Models/Responsable.php
+++ app/Models/Responsable.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Responsable extends Model
 {
+    public $primaryKey = 'cod_trabajador';
     protected $table = 'rhuma_trabajador';
 
     protected $fillable = [
